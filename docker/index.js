a6ff3e2880c7c2dfc8d2d835ed1d0890ebd1f51c
Jeferson Albino <jeferson@cleancloud.com.br>
2023-09-08 19:21:54 -0300

    Removed await statement

--- docker/index.js
+++ docker/index.js
@@ -10,10 +10,10 @@ const decrypt = async (secret) => {
     return response.Plaintext.toString()
 };
 
-process.env.DATABASE_PASSWORD = await decrypt(process.env.DATABASE_PASSWORD);
-process.env.INIT_CLIENT_API_TOKENS = await decrypt(process.env.INIT_CLIENT_API_TOKENS);
-process.env.INIT_FRONTEND_API_TOKENS = await decrypt(process.env.INIT_FRONTEND_API_TOKENS);
-process.env.AUTH0_API_CLIENT_SECRET = await decrypt(process.env.AUTH0_API_CLIENT_SECRET);
+process.env.DATABASE_PASSWORD = decrypt(process.env.DATABASE_PASSWORD);
+process.env.INIT_CLIENT_API_TOKENS = decrypt(process.env.INIT_CLIENT_API_TOKENS);
+process.env.INIT_FRONTEND_API_TOKENS = decrypt(process.env.INIT_FRONTEND_API_TOKENS);
+process.env.AUTH0_API_CLIENT_SECRET = decrypt(process.env.AUTH0_API_CLIENT_SECRET);
 
 let options = {};
 unleash.start(options);
