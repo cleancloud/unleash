17157133d24b8da838a447735dcbb5339364c0ac
Jeferson Albino <jeferson@cleancloud.com.br>
2023-09-08 14:41:44 -0300

    Included missing function statement on entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -eu
 
-decrypt() {
+function decrypt() {
     echo "${1:-}" | base64 --decode > decrypt_blob
     aws kms decrypt --ciphertext-blob fileb://decrypt_blob --query Plaintext --output text | base64 --decode
 }
 
-export DATABASE_PASSWORD="$(decrypt "$DATABASE_PASSWORD")"
+export DATABASE_PASSWORD=$(decrypt "$DATABASE_PASSWORD")
 
-export INIT_CLIENT_API_TOKENS="$(decrypt "$INIT_CLIENT_API_TOKENS")"
+export INIT_CLIENT_API_TOKENS=$(decrypt "$INIT_CLIENT_API_TOKENS")
 
-export INIT_FRONTEND_API_TOKENS="$(decrypt "$INIT_FRONTEND_API_TOKENS")"
+export INIT_FRONTEND_API_TOKENS=$(decrypt "$INIT_FRONTEND_API_TOKENS")
 
-export AUTH0_API_CLIENT_SECRET="$(decrypt "$AUTH0_API_CLIENT_SECRET")"
+export AUTH0_API_CLIENT_SECRET=$(decrypt "$AUTH0_API_CLIENT_SECRET")
 
 node index.js
