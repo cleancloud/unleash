96623fbefd77b027d5ac23697515cf2379362fd1
Jeferson Albino <jeferson@cleancloud.com.br>
2023-09-08 18:55:38 -0300

    Included wrapper function to await for all decryptions

--- docker/index.js
+++ docker/index.js
@@ -4,30 +4,28 @@ const aws = require('aws-sdk');
 const unleash = require('unleash-server');
 
 aws.config.update({ region: process.env.AWS_REGION });
-console.log('AWS SDK version: ', aws.VERSION);
+const kms = new aws.KMS();
 
-async function decryptData(data) {
+async function decrypt(data) {
     const params = {
-        CiphertextBlob: Buffer.from(data, 'base64'),
-        KeyId: process.env.AWS_KMS_ID
+        CiphertextBlob: Buffer.from(data, 'base64')
     };
 
-    console.log("Input: ", data);
-
     try {
-        const decrypted = await new aws.KMS().decrypt(params).promise();
-        console.log("Decrypted: ", decrypted.Plaintext.toString());
+        const decrypted = await kms.decrypt(params).promise();
         return decrypted.Plaintext.toString('utf-8');
     } catch (err) {
         console.error('Error decrypting data: ', err);
     }
 }
 
-process.env.DATABASE_PASSWORD = decryptData(process.env.DATABASE_PASSWORD);
-process.env.INIT_CLIENT_API_TOKENS = decryptData(process.env.INIT_CLIENT_API_TOKENS);
-process.env.INIT_FRONTEND_API_TOKENS = decryptData(process.env.INIT_FRONTEND_API_TOKENS);
-process.env.AUTH0_API_CLIENT_SECRET = decryptData(process.env.AUTH0_API_CLIENT_SECRET);
+async function decryptAll() {
+    process.env.DATABASE_PASSWORD = await decrypt(process.env.DATABASE_PASSWORD);
+    process.env.INIT_CLIENT_API_TOKENS = await decrypt(process.env.INIT_CLIENT_API_TOKENS);
+    process.env.INIT_FRONTEND_API_TOKENS = await decrypt(process.env.INIT_FRONTEND_API_TOKENS);
+    process.env.AUTH0_API_CLIENT_SECRET = await decrypt(process.env.AUTH0_API_CLIENT_SECRET);
+}
 
-
+decryptAll();
 let options = {};
 unleash.start(options);
